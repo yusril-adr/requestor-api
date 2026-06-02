5f3a50198bb4c2d1e282f83acc85f4ac188882a0
Yusril Adriansyah Putra <yusriladr.37@gmail.com>
2026-06-02 14:56:25 +0700

    build: :rocket: add remove ignoreEnv file

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,7 @@ import { AuditLogModule } from '@modules/AuditLog/audit-log.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      ignoreEnvVars: process.env.NODE_ENV === 'production',
     }),
     TypeOrmModule.forRootAsync({
       useFactory: async () => ({
